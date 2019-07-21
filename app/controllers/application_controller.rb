094617fb402fa25fdf5cda3da0203dab4f108979
Nathan Veach <nathanbveach@gmail.com>
2019-07-21 14:04:28 -0400

    Added code comments for Stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+   # Whitelist the following form fields so that we can process them, if coming from
+   # a devise comment form.
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
     def configure_permitted_parameters
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  # Extebd defaykt Devise gem behavior so that 
+  # users signing up with the Pro account (Plan ID)
+  # save with a special Stripe subscription function.
+  # Otherwise Devise signs up user as usual.
   def create
     super do |resource|
       if params[:plan]
